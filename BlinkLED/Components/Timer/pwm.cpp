52ff26b20addbf39c1d6c48ebff87aef532f8fb3
BMinh2109 <youremail@example.com>
2023-09-12 09:17:47 +0700

    thêm 1 lệnh clear flag

--- BlinkLED/Components/Timer/pwm.cpp
+++ BlinkLED/Components/Timer/pwm.cpp
@@ -21,7 +21,7 @@ void pwm::HAL::on(int pulse){
 	LL_TIM_OC_SetCompareCH1(TIM1, pulse);
 		LL_TIM_ClearFlag_UPDATE( TIM1 );
 		while ( LL_TIM_IsActiveFlag_UPDATE( TIM1 ) == 0u ){}
-		//LL_TIM_ClearFlag_UPDATE( TIM1 );
+		LL_TIM_ClearFlag_UPDATE( TIM1 );
 		TIM1->BDTR |= LL_TIM_OSSI_ENABLE;
 		LL_TIM_EnableAllOutputs( TIM1 );
 		LL_TIM_EnableIT_UPDATE( TIM1 );
@@ -31,6 +31,5 @@ extern "C" void TIM1_UP_TIM16_IRQHandler(void)
 {
 	LL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
 	LL_TIM_ClearFlag_UPDATE(TIM1);
-
 }
 
